3919e84e5628700704845d0f976ac2d5e126298c
Surya. N <suryanagarajan30112000@gmail.com>
2026-09-05 16:44:07 +0530

    Commited .env

--- Client/src/utils/apiConfig.js
+++ Client/src/utils/apiConfig.js
@@ -1,3 +1,3 @@
 const defaultApiOrigin = `${window.location.protocol}//${window.location.hostname}:5000`;
 
-export const API_BASE_URL = https://task-management-kkq2.onrender.com/;
+export const API_BASE_URL = import.meta.env.VITE_API_URL || defaultApiOrigin;
